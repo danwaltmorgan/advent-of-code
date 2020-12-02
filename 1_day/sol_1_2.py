939b8b92e7067677f9830cb34bf516693071d9ba
Dan Morgan <ltdan681@gmail.com>
2020-12-01 19:43:20 -0500

    initial commit and first day of code

---
+++ 1_day/sol_1_2.py
@@ -0,0 +1,23 @@
+import os
+path = os.path.dirname(os.path.realpath(__file__))
+
+file = open(os.path.join(path, "input.txt"), "r")
+
+content = file.readlines()
+
+list = []
+
+for line in content:
+    list.append(int(line))
+    
+for i in list:
+    for j in list:
+        if (i + j == 2020):
+            print(i,j, i* j)
+
+
+for i in list:
+    for j in list:
+        for k in list:
+            if (i + j + k == 2020):
+                print(i,j,k, i * j * k)
